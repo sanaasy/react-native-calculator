3ca1133d816ce6dfc072906844caf155c05a2df5
Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>
2021-03-20 20:32:16 -0400

    Inital push for calculator

---
+++ screens/CalculatorScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class CalculatorScreen extends React.Component {
+  render() {
+    return (
+      <View style={{paddingTop:50}}>
+        <Text>Calculator Screen</Text>
+      </View>
+    )
+  }
+}
