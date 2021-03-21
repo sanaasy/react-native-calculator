70374179bc8e5881cd0af1b45d203a4539cf9502
Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>
2021-03-20 21:52:00 -0400

    Add functionality for each digit and operator

--- components/CalculatorButton.js
+++ components/CalculatorButton.js
@@ -8,6 +8,8 @@ export default class CalculatorButton extends React.Component {
     title: "",
     color: "white",
     backgroundColor: "black",
+    radius: 40,
+    
     style: { },
   }
 
--- screens/CalculatorScreen.js
+++ screens/CalculatorScreen.js
@@ -23,6 +23,36 @@ export default class CalculatorScreen extends React.Component {
     this.calc = new global.swisscalc.calc.calculator();
   }
 
+  onDigitPress = (digit) => {
+    this.calc.addDigit(digit);
+    this.setState({ display: this.calc.getMainDisplay() });
+  }
+
+  onClearPress = () => {
+    this.calc.clear();
+    this.setState({ display: this.calc.getMainDisplay() });
+  }
+
+  onPlusMinusPress = () => {
+    this.calc.negate();
+    this.setState({ display: this.calc.getMainDisplay() });
+  }
+
+  onBinaryOperatorPress = (operator) => {
+    this.calc.addBinaryOperator(operator);
+    this.setState({ display: this.calc.getMainDisplay() });
+  }
+
+  onEqualsPress = () => {
+    this.calc.equalsPressed();
+    this.setState({ display: this.calc.getMainDisplay() });
+  }
+
+  onUnaryOperatorPress = (operator) => {
+    this.calc.addUnaryOperator(operator);
+    this.setState({ display: this.calc.getMainDisplay() });
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -31,39 +61,39 @@ export default class CalculatorScreen extends React.Component {
           <CalculatorDisplay display={this.state.display} />
         </View>
 
-        <View>
+        <View style={styles.buttonContainer}>
           <View style={styles.buttonRow}>
-            <CalculatorButton title="C" color="red" backgroundColor="blue" />
-            <CalculatorButton title="+/-" color="red" backgroundColor="blue" />
-            <CalculatorButton title="%" color="red" backgroundColor="blue" />
-            <CalculatorButton title="÷" color="red" backgroundColor="blue" />
+            <CalculatorButton onPress={this.onClearPress} title="C" color="black" backgroundColor="#D4D4D2" />
+            <CalculatorButton onPress={this.onPlusMinusPress} title="+/-" color="black" backgroundColor="#D4D4D2" />
+            <CalculatorButton onPress={() => { this.onUnaryOperatorPress(this.oc.PercentOperator) }} title="%" color="black" backgroundColor="#D4D4D2" />
+            <CalculatorButton onPress={() => { this.onBinaryOperatorPress(this.oc.DivisionOperator) }} title="÷" color="white" backgroundColor="#FF9500" />
           </View>
 
           <View style={styles.buttonRow}>
-            <CalculatorButton title="7" color="red" backgroundColor="blue" />
-            <CalculatorButton title="8" color="red" backgroundColor="blue" />
-            <CalculatorButton title="9" color="red" backgroundColor="blue" />
-            <CalculatorButton title="×" color="red" backgroundColor="blue" />
+            <CalculatorButton onPress={() => { this.onDigitPress("7") }} title="7" color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={() => { this.onDigitPress("8") }} title="8" color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={() => { this.onDigitPress("9") }} title="9" color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={() => { this.onBinaryOperatorPress(this.oc.MultiplicationOperator) }} title="×" color="white" backgroundColor="#FF9500" />
           </View>
           
           <View style={styles.buttonRow}>
-            <CalculatorButton title="4" color="red" backgroundColor="blue" />
-            <CalculatorButton title="5" color="red" backgroundColor="blue" />
-            <CalculatorButton title="6" color="red" backgroundColor="blue" />
-            <CalculatorButton title="-" color="red" backgroundColor="blue" />
+            <CalculatorButton onPress={() => { this.onDigitPress("4") }} title="4" color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={() => { this.onDigitPress("5") }} title="5" color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={() => { this.onDigitPress("6") }} title="6" color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={() => { this.onBinaryOperatorPress(this.oc.SubtractionOperator) }} title="-" color="white" backgroundColor="#FF9500" />
           </View>
 
           <View style={styles.buttonRow}>
-            <CalculatorButton title="1" color="red" backgroundColor="blue" />
-            <CalculatorButton title="2" color="red" backgroundColor="blue" />
-            <CalculatorButton title="3" color="red" backgroundColor="blue" />
-            <CalculatorButton title="+" color="red" backgroundColor="blue" />
+            <CalculatorButton onPress={() => { this.onDigitPress("1") }} title="1" color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={() => { this.onDigitPress("2") }} title="2" color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={() => { this.onDigitPress("3") }} title="3" color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={() => { this.onBinaryOperatorPress(this.oc.AdditionOperator) }} title="+" color="white" backgroundColor="#FF9500" />
           </View>
 
           <View style={styles.buttonRow}>
-            <CalculatorButton title="0" color="red" backgroundColor="blue" style={{ flex: 2 }} />
-            <CalculatorButton title="." color="red" backgroundColor="blue" />
-            <CalculatorButton title="=" color="red" backgroundColor="blue" />
+            <CalculatorButton onPress={() => { this.onDigitPress("0") }} title="0" color="white" backgroundColor="#1C1C1C" style={{ width: 190 }} />
+            <CalculatorButton onPress={() => { this.onDigitPress(".") }} title="." color="white" backgroundColor="#1C1C1C" />
+            <CalculatorButton onPress={this.onEqualsPress} title="=" color="white" backgroundColor="#FF9500" />
           </View>
         </View>
 
@@ -80,6 +110,9 @@ const styles = StyleSheet.create({
   displayContainer: { 
     flex: 1, 
     justifyContent: "flex-end",
+  },
+  buttonContainer: {
+    paddingBottom: 20,
   },
   buttonRow: { 
     flexDirection: "row", 
